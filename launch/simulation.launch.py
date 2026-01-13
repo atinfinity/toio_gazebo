a3d55188456ab7bfe71e23930f2e6c603ec9e800
atinfinity <dandelion1124@gmail.com>
2026-01-13 16:53:38 +0900

    changed x_pose and y_pose

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -52,8 +52,8 @@ def generate_launch_description():
     world = LaunchConfiguration('world')
     world_frame = LaunchConfiguration('world_frame')
     pose = {
-        'x': LaunchConfiguration('x_pose', default='0.00'),
-        'y': LaunchConfiguration('y_pose', default='0.00'),
+        'x': LaunchConfiguration('x_pose', default='0.05'),
+        'y': LaunchConfiguration('y_pose', default='-0.05'),
         'z': LaunchConfiguration('z_pose', default='0.01'),
         'R': LaunchConfiguration('roll', default='0.00'),
         'P': LaunchConfiguration('pitch', default='0.00'),
